2a19de715a890480b3c4f928738e56cab04bb279
MDN <mark.denardo@gmail.com>
2024-01-25 18:41:23 -0500

    Update wall_spawn.js

--- wall_spawn.js
+++ wall_spawn.js
@@ -8,21 +8,22 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function spawn(){
+function spawn() {
   x = getRandomInt(10);
   y = getRandomInt(10);
   x1 = getRandomInt(10);
   y1 = getRandomInt(10);
   cord = [x, y];
   bound = [x1, y1];
-  console.log(sameMembers(cord,bound))
+  console.log(sameMembers(cord, bound));
 }
 
 function sameMembers(arr1, arr2) {
-    const set1 = new Set(arr1);
-    const set2 = new Set(arr2);
-    return arr1.every(item => set2.has(item)) &&
-        arr2.every(item => set1.has(item))
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+  return (
+    arr1.every((item) => set2.has(item)) && arr2.every((item) => set1.has(item))
+  );
 }
 
 function setup() {
@@ -30,5 +31,5 @@ function setup() {
 }
 
 function draw() {
-  spawn()
+  spawn();
 }
